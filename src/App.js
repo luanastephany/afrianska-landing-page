ec5838b47f39b1bc2d0cb444c2479a6e1e2409cc
Luana Stephany <ls.luanastephany@gmail.com>
2021-09-01 10:05:25 -0300

    atualizando pastas da raiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
+import Header from './components/Header'
+import Hero from './sections/Hero/Hero';
 
 function App() {
   return (
     <main>
-
+      <Header />
+      <Hero />
 
 
     </main>
