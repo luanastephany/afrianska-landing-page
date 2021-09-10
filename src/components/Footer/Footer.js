c6353bcfbb64fe96ba48b57babacc494b6bdf6bf
Luana Stephany <ls.luanastephany@gmail.com>
2021-09-10 15:18:14 -0300

    criando componente footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Footer.scss'
+import Button from '../Button'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <section className="first-section">
+        <div className="footer-texts">
+          <h1 className="footer-title">Interested to work with our team?</h1>
+          <p className="footer-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        </div>
+        <Button>Lets Talk</Button>
+      </section>
+
+
+      <section className="second-section"></section>
+
+
+
+    </div>
+  )
+}
+
+export default Footer
